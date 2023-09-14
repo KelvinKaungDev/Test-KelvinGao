b05994575437b7e8f5078e48c93035c5dfab95f3
Kelvin Gao <u6511753@au.edu>
2023-09-15 00:51:18 +0700

    Initial Commit

---
+++ Test-KelvinGao/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-KelvinGao
+//
+//  Created by Kelvin Gao  on 15/9/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
